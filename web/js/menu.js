68296296f3ed3d54ee94c2ff90f8b1b813772faf
Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
2023-02-09 21:12:35 +0000

    Added Menu for Mobile and Tablets

---
+++ web/js/menu.js
@@ -0,0 +1,17 @@
+const menucontent = document.querySelectorAll(".menu_content");
+
+menucontent.forEach(element => {
+    element.style.transition = "color .3s ease-in-out";
+});
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    menucontent.forEach(element => {element.style.color = "#F8F9FA";});
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+    setTimeout(() => {
+        menucontent.forEach(element => {element.style.color = "black";});
+    }, 300);
+  }

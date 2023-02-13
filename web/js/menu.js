0dfd25bb3893db66169f9795fdaea0ccfefa73fe
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2023-02-13 23:23:20 +0000

    fix: Corrección de error en el acceso a elementos
    Correción del nombre mal escrito en el método getElementById, y eliminación de paréntesis innecesarios

--- web/js/menu.js
+++ web/js/menu.js
@@ -4,8 +4,8 @@ menucontent.forEach((element) => {
   element.style.transition = "color .3s ease-in-out";
 });
 
-const oNav = document.getElementById("opeanNav");
-oNav.addEventListener("click", openNav());
+const oNav = document.getElementById("openNav");
+oNav.addEventListener("click", openNav);
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
   menucontent.forEach((element) => {
@@ -14,7 +14,7 @@ function openNav() {
 }
 
 const cNav = document.getElementById("closeNav");
-cNav.addEventListener("click", closeNav());
+cNav.addEventListener("click", closeNav);
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
   setTimeout(() => {

ffee31705430d089c5f94355c5e814d3f992acec
Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
2023-02-13 19:20:02 +0000

    Menu 1.0.2

--- web/js/menu.js
+++ web/js/menu.js
@@ -1,17 +1,21 @@
 const menucontent = document.querySelectorAll(".menu_content");
 
-menucontent.forEach(element => {
-    element.style.transition = "color .3s ease-in-out";
+menucontent.forEach((element) => {
+  element.style.transition = "color .3s ease-in-out";
 });
 
 function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-    menucontent.forEach(element => {element.style.color = "#F8F9FA";});
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-    setTimeout(() => {
-        menucontent.forEach(element => {element.style.color = "black";});
-    }, 300);
-  }
+  document.getElementById("myNav").style.height = "100%";
+  menucontent.forEach((element) => {
+    element.style.color = "#F8F9FA";
+  });
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+  setTimeout(() => {
+    menucontent.forEach((element) => {
+      element.style.color = "black";
+    });
+  }, 300);
+}
